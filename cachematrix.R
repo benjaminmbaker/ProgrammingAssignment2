5d3261e2c8e40d2ddbd53c982407b6817b8b7b16
Benjamin M Baker <benjaminmbaker@gmail.com>
2015-01-24 17:36:42 -0800

    Completed assignment for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example usage:
+## x <- matrix(rnorm(16), nrow=4)       -> Creating matrix for processing
+## x.cache <- makeCacheMatrix(x)        -> Create cached matrix
+## x.cache$get                          -> Return cached matrix
+## cacheSolve(x.cache)                  -> Invert matrix
+## 
 
-## Write a short comment describing this function
+##  makeCacheMatrix: return a list of functions to:
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse
+## 4. Get the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        # Create value for cached matrix input
+        inv <- NULL
+        # Set function the matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        # Get function for the matrix
+        get <- function() x
+        # Set function for the inverse
+        setinverse <- function(inverse) inv <<- inverse
+        # Get function for the inverse
+        getinverse <- function() i
+        
+        # Return matrix with defined functions
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        
+        # If inverse is already cached, return it.
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        # Calculating inverse
+        data <- x$get()
+        i <- solve(data, ...)
+        
+        # Cache inverse
+        x$setinverse(i)
+        
+        # Call inverse
+        i
 }
